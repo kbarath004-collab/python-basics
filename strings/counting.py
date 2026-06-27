2be80b5229078184a4f1ea7c419a1ccbdc215e67
HP <barathfkumar00712@gmail.com>
2026-06-27 23:35:59 +0530

    feat(strings): added string manipulation exercises

---
+++ strings/counting.py
@@ -0,0 +1,5 @@
+s = "textual unit consisting of one or more words that are grammatically linked, expressing a complete thought in non-functional linguistics"
+print(len(s.split()))
+print(len(s))
+print("first word is: ", s.split()[0])
+print("last word is: ", s.split()[-1])
---
+++ strings/reporting.py
@@ -0,0 +1,2 @@
+s = "textual unit consisting of one or more words that are grammatically linked, expressing a complete thought in non-functional linguistics"
+print(s.count("4"))
